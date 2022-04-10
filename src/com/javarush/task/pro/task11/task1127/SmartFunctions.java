b0dfdf5ff556ecb4bc190f9ed51123f687d8ae3d
Leonid_Khristyuk <kasheyne@gmail.com>
2022-04-10 21:27:11 +0300

    Level 4 solutions

---
+++ src/com/javarush/task/pro/task11/task1127/SmartFunctions.java
@@ -0,0 +1,38 @@
+package com.javarush.task.pro.task11.task1127;
+
+/*
+Телефон в наследство
+*/
+
+public interface SmartFunctions extends BasicFunctions {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
