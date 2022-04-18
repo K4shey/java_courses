bd268938fdbc9a060f8e27a22406c413a9c82c43
Leonid_Khristyuk <kasheyne@gmail.com>
2022-04-18 07:29:06 +0300

    Refactoring. Add exception handler.

--- src/com/javarush/task/pro/task15/task1504/Solution.java
+++ src/com/javarush/task/pro/task15/task1504/Solution.java
@@ -10,30 +10,30 @@ import java.util.Scanner;
 */
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter source filename:");
         String sourceFileName = scanner.nextLine();
         System.out.println("Enter destination filename:");
         String destinationFileName = scanner.nextLine();
+        try (InputStream inputStream = Files.newInputStream(Paths.get(sourceFileName));
+             OutputStream outputStream = Files.newOutputStream(Paths.get(destinationFileName))){
+            byte[] array = new byte[2];
+            while (inputStream.available() > 0) {
 
-        InputStream inputStream = Files.newInputStream(Paths.get(sourceFileName));
-        OutputStream outputStream = Files.newOutputStream(Paths.get(destinationFileName));
-        byte[] array = new byte[2];
-        while (inputStream.available() > 0) {
-
-           int num =  inputStream.read(array);
-           if (num > 1) {
-               byte tmp = array[0];
-               array[0] = array[1];
-               array[1] = tmp;
-               outputStream.write(array,0,2);
-            } else {
-               outputStream.write(array,0,1);
-           }
+                int num =  inputStream.read(array);
+                if (num > 1) {
+                    byte tmp = array[0];
+                    array[0] = array[1];
+                    array[1] = tmp;
+                    outputStream.write(array,0,2);
+                } else {
+                    outputStream.write(array,0,1);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Something went wrong " + e);
         }
-        inputStream.close();
-        outputStream.close();
     }
 }
 
