acff7c5bee9a43d00abf586e473717437eae7536
Leonid_Khristyuk <kasheyne@gmail.com>
2022-04-20 23:59:03 +0300

    Level 10, lecture 7 solutions

---
+++ src/com/javarush/task/pro/task17/task1716/Vehicle.java
@@ -0,0 +1,46 @@
+package com.javarush.task.pro.task17.task1716;
+
+/*
+Дорожное движение
+*/
+
+public interface Vehicle {
+    default void start() {
+        System.out.println("Начинаю движение.");
+    }
+    void move();
+
+   default public void stop() {
+        System.out.println("Останавливаюсь.");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/com/javarush/task/pro/task17/task1717/Flyable.java
@@ -0,0 +1,40 @@
+package com.javarush.task.pro.task17.task1717;
+
+/*
+Кто летит?
+*/
+
+public interface Flyable {
+   int getMaxSpeed();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
