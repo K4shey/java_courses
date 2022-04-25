7b77c37a12f5b78120e06712a5c355de1ac5f61f
Leonid_Khristyuk <kasheyne@gmail.com>
2022-04-25 13:01:01 +0300

    Java core, Level 2, lecture 4 solutions

---
+++ src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task12.task1207;
+
+/*
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Integer value = 5;
+        print(value);
+        print(value.intValue());
+    }
+
+    public static void print(int value) {
+        System.out.println("int " + value);
+    }
+
+    public static void print(Integer value) {
+        System.out.println("Integer " + value);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,62 @@
+package com.javarush.task.task12.task1208;
+
+/*
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(Integer i) {
+
+    }
+
+    public static void print(int i) {
+
+    }
+
+    public static void print(double d) {
+
+    }
+
+    public static void print(String s) {
+
+    }
+
+    public static void print(float f) {
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
