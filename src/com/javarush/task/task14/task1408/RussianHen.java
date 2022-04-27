c1d044848ebf246be0b4191a5991280b3d33b906
Leonid_Khristyuk <kasheyne@gmail.com>
2022-04-27 13:19:22 +0300

    Java core, Level 4, lecture 6 solution

---
+++ src/com/javarush/task/task14/task1408/RussianHen.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task14.task1408;
+
+public class RussianHen extends Hen {
+
+    public int getCountOfEggsPerMonth() {
+        return 5;
+    }
+
+    public String getDescription() {
+        return String.format("%s Моя страна - %s. Я несу %d яиц в месяц.",
+                super.getDescription(), Country.RUSSIA, getCountOfEggsPerMonth());
+    }
+}
