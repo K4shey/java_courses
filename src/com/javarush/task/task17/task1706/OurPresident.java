0da7cfe7ac402e8630dbfc4a7b68bf7a10351e59
Leonid_Khristyuk <kasheyne@gmail.com>
2022-05-03 10:48:43 +0300

    Java core, Level 7, lecture 4 solutions

---
+++ src/com/javarush/task/task17/task1706/OurPresident.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task17.task1706;
+
+public class OurPresident {
+    private static OurPresident president;
+
+    static {
+         if (president == null) {
+           synchronized (OurPresident.class) {
+//                synchronized (president) {
+                    president = new OurPresident();
+            }
+        }
+    }
+
+    private OurPresident() {
+    }
+
+    public static OurPresident getOurPresident() {
+        return president;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
