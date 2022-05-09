e04480c059e0742a39777bfb5814db2b87426fa2
Leonid_Khristyuk <kasheyne@gmail.com>
2022-05-09 18:05:37 +0300

    Java core, Level 8, lecture 11 solutions

--- src/com/javarush/task/task18/task1825/Solution.java
+++ src/com/javarush/task/task18/task1825/Solution.java
@@ -10,36 +10,27 @@ import java.util.TreeSet;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Set<String> files = new TreeSet<>();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String nextLine = bufferedReader.readLine();
+        while (!nextLine.equals("end")) {
+            files.add(nextLine);
+            nextLine = bufferedReader.readLine();
+        }
+        bufferedReader.close();
+        String anyFile = files.iterator().next();
+        String newName = anyFile.substring(0, anyFile.indexOf(".part"));
+        FileOutputStream fileOutputStream = new FileOutputStream(newName);
+        for (String filename : files) {
+            FileInputStream fileInputStream = new FileInputStream(filename);
+            byte[] buffer = new byte[1024];
+            while (fileInputStream.available() > 0) {
+                int count = fileInputStream.read(buffer);
+                fileOutputStream.write(buffer, 0, count);
+            }
+            fileInputStream.close();
+        }
+        fileOutputStream.close();
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
