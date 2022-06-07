1acfa9d153fe3537e7be9e9712bd3acc2d27a49d
Leonid_Khristyuk <kasheyne@gmail.com>
2022-06-07 21:51:39 +0300

    Multi threading, Level 3, lecture 13 solutions

--- src/com/javarush/task/task24/task2413/Ball.java
+++ src/com/javarush/task/task24/task2413/Ball.java
@@ -49,4 +49,15 @@ public class Ball extends BaseObject {
     public double getDy() {
         return dy;
     }
+
+    public void setDirection(double direction) {
+        this.direction = direction;
+        double angle = Math.toRadians(direction);
+        dx = Math.cos(angle) * speed;
+        dy = -Math.sin(angle) * speed;
+    }
+
+    void checkRebound(int minx, int maxx, int miny, int maxy) {
+
+    }
 }
