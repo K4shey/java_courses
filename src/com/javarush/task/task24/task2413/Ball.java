871cacfff2ffa4aeca235c50c83c96059ffed177
Leonid_Khristyuk <kasheyne@gmail.com>
2022-06-07 08:40:28 +0300

    Multi threading, Level 3, lecture 13 solutions

--- src/com/javarush/task/task24/task2413/Ball.java
+++ src/com/javarush/task/task24/task2413/Ball.java
@@ -1,8 +1,19 @@
 package com.javarush.task.task24.task2413;
 
 public class Ball extends BaseObject {
-    public Ball(double x, double y, double radius) {
-        super(x, y, radius);
+
+    private double speed;
+    private double direction;
+    private double dx;
+    private double dy;
+    private boolean isFrozen;
+
+
+    public Ball(double x, double y, double speed, double direction) {
+        super(x, y, 1);
+        this.speed = speed;
+        this.direction = direction;
+        this.isFrozen = true;
     }
 
     @Override
@@ -13,5 +24,21 @@ public class Ball extends BaseObject {
     @Override
     public void move() {
 
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public double getDx() {
+        return dx;
+    }
+
+    public double getDy() {
+        return dy;
     }
 }
