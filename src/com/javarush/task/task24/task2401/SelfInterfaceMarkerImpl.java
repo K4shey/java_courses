2a12e031b370b9e34cee74d075dfb62fee09f420
Leonid_Khristyuk <kasheyne@gmail.com>
2022-06-08 08:02:29 +0300

    Multi threading, Level 4, lecture 2 solutions

---
+++ src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task24.task2401;
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker {
+
+    public void printHello() {
+        System.out.print("hello ");
+    }
+
+    public void printWorld() {
+        System.out.print(" world!");
+    }
+
+}
---
+++ src/com/javarush/task/task24/task2401/Solution.java
@@ -0,0 +1,47 @@
+package com.javarush.task.task24.task2401;
+
+/*
+Создание своего интерфейса-маркера
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) throws UnsupportedInterfaceMarkerException {
+        SelfInterfaceMarkerImpl obj = new SelfInterfaceMarkerImpl();
+        Util.testClass(obj);
+//        Util.testClass(null);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
