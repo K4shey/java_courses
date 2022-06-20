ab2ec78de693050f82356949b58fea9e2006e245
Leonid_Khristyuk <kasheyne@gmail.com>
2022-06-20 22:05:39 +0300

    Multi threading, Level 4, lecture 14 solutions

---
+++ src/com/javarush/task/task36/task3608/model/FakeModel.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FakeModel implements Model {
+
+    private ModelData modelData = new ModelData();
+
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    public void loadUsers() {
+        List<User> users = new ArrayList<>();
+        users.add(new User("Berezovsky", 1, 1));
+        users.add(new User("Khodorkovsky", 2, 1));
+        users.add(new User("Deripaska", 3, 2));
+        users.add(new User("Miller", 4, 2));
+        users.add(new User("Sechin", 5, 3));
+
+        modelData.setUsers(users);
+    }
+}
