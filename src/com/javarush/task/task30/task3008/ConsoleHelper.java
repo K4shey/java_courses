cd1ebd8c98e69249051c87edaf5920a0a7d61533
Leonid_Khristyuk <kasheyne@gmail.com>
2022-07-17 10:15:32 +0300

    Multi threading, Level 6, lecture 15 solutions.

--- src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -1,4 +1,38 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        while (true) {
+            try {
+                String result = bufferedReader.readLine();
+                if (result != null) {
+                    return result;
+                }
+            } catch (IOException ioe) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз");
+            }
+        }
+    }
+
+    public static int readInt() {
+        while (true) {
+            try {
+                int result = Integer.parseInt(readString());
+                return result;
+            } catch (NumberFormatException nfe) {
+                System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз");
+            }
+        }
+    }
+
 }
