e6874b842b2b4cc6dfb44b72a061d4c637df9cc9
Leonid_Khristyuk <kasheyne@gmail.com>
2022-07-18 15:51:48 +0300

    Multi threading, Level 6, lecture 15 solutions.

--- src/com/javarush/task/task30/task3008/Server.java
+++ src/com/javarush/task/task30/task3008/Server.java
@@ -1,10 +1,29 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
     public static void main(String[] args) {
-
+        int serverPort = ConsoleHelper.readInt();
+        System.out.println("Server started");
+        try (ServerSocket serverSocket = new ServerSocket(serverPort)) {
+            while (true) {
+                new Handler(serverSocket.accept()).start();
+            }
+        } catch (IOException ioe) {
+            System.out.println("Something went wrong!");
+        }
     }
 
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+    }
 }
 
 
