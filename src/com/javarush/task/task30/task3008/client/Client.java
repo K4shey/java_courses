1b28e1a0ab7bdcbec426be50da85187a4f511bd2
Leonid_Khristyuk <kasheyne@gmail.com>
2022-07-21 07:59:48 +0300

    Multi threading, Level 6, lecture 15 solutions.

--- src/com/javarush/task/task30/task3008/client/Client.java
+++ src/com/javarush/task/task30/task3008/client/Client.java
@@ -1,14 +1,51 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.IOException;
 
 public class Client {
 
     protected Connection connection;
     volatile private boolean clientConnected = false;
 
+    protected String getServerAddress() {
+        ConsoleHelper.writeMessage("Enter address of server");
+        return ConsoleHelper.readString();
+    }
 
-    public class SocketThread extends Thread{
+    protected int getServerPort(){
+        ConsoleHelper.writeMessage("Enter port of server");
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName(){
+        ConsoleHelper.writeMessage("Enter user's name");
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole() {
+        return true;
+    }
+
+    protected SocketThread getSocketThread(){
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text){
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException ioe) {
+            ConsoleHelper.writeMessage("Message sending error");
+            clientConnected = false;
+        }
+
+    }
+
+    public class SocketThread extends Thread {
 
     }
 }
