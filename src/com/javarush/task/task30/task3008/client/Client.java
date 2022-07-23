a62391a3176ae15b4b57e00d13793a08c13fbd44
Leonid_Khristyuk <kasheyne@gmail.com>
2022-07-23 14:02:28 +0300

    Multi threading, Level 6, lecture 15 solutions.

--- src/com/javarush/task/task30/task3008/client/Client.java
+++ src/com/javarush/task/task30/task3008/client/Client.java
@@ -17,12 +17,12 @@ public class Client {
         return ConsoleHelper.readString();
     }
 
-    protected int getServerPort(){
+    protected int getServerPort() {
         ConsoleHelper.writeMessage("Enter port of server");
         return ConsoleHelper.readInt();
     }
 
-    protected String getUserName(){
+    protected String getUserName() {
         ConsoleHelper.writeMessage("Enter user's name");
         return ConsoleHelper.readString();
     }
@@ -31,11 +31,11 @@ public class Client {
         return true;
     }
 
-    protected SocketThread getSocketThread(){
+    protected SocketThread getSocketThread() {
         return new SocketThread();
     }
 
-    protected void sendTextMessage(String text){
+    protected void sendTextMessage(String text) {
         try {
             connection.send(new Message(MessageType.TEXT, text));
         } catch (IOException ioe) {
@@ -48,4 +48,39 @@ public class Client {
     public class SocketThread extends Thread {
 
     }
+
+    public void run() {
+        SocketThread socketThread = getSocketThread();
+        socketThread.setDaemon(true);
+        socketThread.start();
+        synchronized (this) {
+            try {
+                this.wait();
+            } catch (InterruptedException ie) {
+                ConsoleHelper.writeMessage("Something went wrong");
+                System.exit(0);
+            }
+        }
+        if (clientConnected) {
+            ConsoleHelper.writeMessage("Соединение установлено. Для выхода наберите команду 'exit'.");
+        } else {
+            ConsoleHelper.writeMessage("Произошла ошибка во время работы клиента");
+        }
+
+        while (clientConnected) {
+            String text = ConsoleHelper.readString();
+            if (text.equals("exit")) {
+                break;
+            }
+            if (shouldSendTextFromConsole()) {
+                sendTextMessage(text);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
+    }
+
 }
