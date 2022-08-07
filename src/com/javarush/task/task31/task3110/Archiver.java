930ea8194fb1d5fd70f56ef79b3363b7852c61e6
Leonid_Khristyuk <kasheyne@gmail.com>
2022-08-07 13:02:25 +0300

    Multi threading, Level 7, lecture 15 solutions.

---
+++ src/com/javarush/task/task31/task3110/Archiver.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Archiver {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter path to Zip file:");
+        String pathToZip = bufferedReader.readLine();
+        System.out.println("Enter path to file to archive:");
+        String pathToFile = bufferedReader.readLine();
+        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(pathToZip));
+        zipFileManager.createZip(Paths.get(pathToFile));
+    }
+}
