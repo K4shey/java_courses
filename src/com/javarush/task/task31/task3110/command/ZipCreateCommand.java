ea031332d7ae38dbf7dd15a8cdd75df04bb711df
Leonid_Khristyuk <kasheyne@gmail.com>
2022-08-09 07:55:22 +0300

    Multi threading, Level 7, lecture 15 solutions.

---
+++ src/com/javarush/task/task31/task3110/command/ZipCreateCommand.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task31.task3110.command;
+
+public class ZipCreateCommand extends ZipCommand {
+    public void execute() {
+
+    }
+}
---
+++ src/com/javarush/task/task31/task3110/command/ZipRemoveCommand.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task31.task3110.command;
+
+public class ZipRemoveCommand extends ZipCommand {
+    public void execute() {
+
+    }
+}
