d0e9b662b16edf689936e9c3e8c499071d7cc07a
Leonid_Khristyuk <kasheyne@gmail.com>
2022-08-11 20:56:07 +0300

    Multi threading, Level 7, lecture 15 solutions.

--- src/com/javarush/task/task31/task3110/command/ZipCommand.java
+++ src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -1,5 +1,15 @@
 package com.javarush.task.task31.task3110.command;
 
-public abstract class ZipCommand implements Command{
-    public abstract void execute();
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
+
+import java.nio.file.Paths;
+
+public abstract class ZipCommand implements Command {
+    public abstract void execute() throws Exception;
+    public ZipFileManager getZipFileManager() throws Exception{
+        ConsoleHelper.writeMessage("Введите полный путь к архиву");
+        String fullPath = ConsoleHelper.readString();
+        return new ZipFileManager(Paths.get(fullPath));
+    }
 }
