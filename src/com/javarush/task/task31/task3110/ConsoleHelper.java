a78135cb42c0360e894afee94e60ef6680877a49
Leonid_Khristyuk <kasheyne@gmail.com>
2022-08-12 06:51:06 +0300

    Multi threading, Level 7, lecture 15 solutions.

--- src/com/javarush/task/task31/task3110/ConsoleHelper.java
+++ src/com/javarush/task/task31/task3110/ConsoleHelper.java
@@ -5,27 +5,48 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ConsoleHelper {
-    public static void writeMessage(String message) throws IOException {
+    private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
         System.out.println(message);
     }
 
     public static String readString() throws IOException {
-        String result = null;
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        ) {
-            while (result == null) {
-                if (bufferedReader.ready()) {
-                    result = bufferedReader.readLine();
-                }
-            }
-        }
-        return result;
+        String text = bis.readLine();
+        return text;
     }
 
     public static int readInt() throws IOException {
-        int result = 0;
-        result = Integer.parseInt(readString());
-        return result;
+        String text = readString();
+        return Integer.parseInt(text.trim());
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
+++ src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
@@ -1,9 +1,23 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.FileProperties;
+import com.javarush.task.task31.task3110.ZipFileManager;
+import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class ZipContentCommand extends ZipCommand {
     @Override
-    public void execute() {
-
+    public void execute() throws Exception {
+        ConsoleHelper.writeMessage("Просмотр содержимого архива.");
+        ZipFileManager zipFileManager = getZipFileManager();
+        List<FileProperties> filePropertiesList = zipFileManager.getFilesList();
+        for (FileProperties property : filePropertiesList) {
+            ConsoleHelper.writeMessage(property.toString());
+        }
+        ConsoleHelper.writeMessage("Содержимое архива прочитано.");
     }
-
 }
