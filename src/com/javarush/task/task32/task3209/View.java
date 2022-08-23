d545cd9c20022aade09a32f787b42ab31e8005c5
Leonid_Khristyuk <kasheyne@gmail.com>
2022-08-23 10:37:45 +0300

    Multi threading, Level 8, lecture 15 solutions.

--- src/com/javarush/task/task32/task3209/View.java
+++ src/com/javarush/task/task32/task3209/View.java
@@ -1,6 +1,9 @@
 package com.javarush.task.task32.task3209;
 
+import com.javarush.task.task32.task3209.listeners.FrameListener;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -23,12 +26,29 @@ public class View extends JFrame implements ActionListener {
 
     }
 
-    public void init() {
+    public void exit() {
+        controller.exit();
+    }
+
+    public void initMenuBar() {
 
     }
 
-    public void exit() {
-        controller.exit();
+    public void initEditor() {
+
+    }
+
+    public void initGui() {
+        initMenuBar();
+        initEditor();
+        pack();
+    }
+
+    public void init() {
+        initGui();
+        FrameListener frameListener = new FrameListener(this);
+        addWindowListener(frameListener);
+        setVisible(true);
     }
 
 }
