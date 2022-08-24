3f41ba7602aa2442ef2ff1aa21ed222c1a37b2d4
Leonid_Khristyuk <kasheyne@gmail.com>
2022-08-24 08:52:53 +0300

    Multi threading, Level 8, lecture 15 solutions.

--- src/com/javarush/task/task32/task3209/View.java
+++ src/com/javarush/task/task32/task3209/View.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
+import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
 import java.awt.*;
@@ -35,7 +36,15 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void initEditor() {
-
+        htmlTextPane.setContentType("text/html");
+        JScrollPane jScrollPaneHtml = new JScrollPane(htmlTextPane);
+        tabbedPane.addTab("HTML", jScrollPaneHtml);
+        JScrollPane jScrollPaneText = new JScrollPane(plainTextPane);
+        tabbedPane.addTab("Текст", jScrollPaneText);
+        tabbedPane.setPreferredSize(new Dimension(450, 600));
+        TabbedPaneChangeListener tabbedPaneChangeListener = new TabbedPaneChangeListener(this);
+        tabbedPane.addChangeListener(tabbedPaneChangeListener);
+        getContentPane().add(tabbedPane, BorderLayout.CENTER);
     }
 
     public void initGui() {
