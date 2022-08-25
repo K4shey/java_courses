c4ed3fd3fe431ccf60e83c1d0135717d57b364af
Leonid_Khristyuk <kasheyne@gmail.com>
2022-08-25 20:44:15 +0300

    Multi threading, Level 8, lecture 15 solutions.

--- src/com/javarush/task/task32/task3209/Controller.java
+++ src/com/javarush/task/task32/task3209/Controller.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task32.task3209;
 
+import javax.swing.text.StyledEditorKit;
 import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
 
 public class Controller {
@@ -36,6 +38,16 @@ public class Controller {
 
     public void setDocument(HTMLDocument document) {
         this.document = document;
+    }
+
+    public void resetDocument() {
+        if (document != null) {
+            document.removeUndoableEditListener(view.getUndoListener());
+        }
+        HTMLEditorKit htmlEditorKit = new HTMLEditorKit();
+        document = (HTMLDocument) htmlEditorKit.createDefaultDocument();
+        document.addUndoableEditListener(view.getUndoListener());
+        view.update();
     }
 }
 
