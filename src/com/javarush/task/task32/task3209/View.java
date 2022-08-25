bf5c89e0b6aad460853ecaac741f5e8c9ee52e52
Leonid_Khristyuk <kasheyne@gmail.com>
2022-08-25 13:02:28 +0300

    Multi threading, Level 8, lecture 15 solutions.

--- src/com/javarush/task/task32/task3209/View.java
+++ src/com/javarush/task/task32/task3209/View.java
@@ -82,4 +82,13 @@ public class View extends JFrame implements ActionListener {
 
     }
 
+    public boolean canUndo(){
+        return false;
+    }
+
+    public boolean canRedo(){
+        return false;
+    }
+
+
 }
