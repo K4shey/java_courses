f8f83af139464bda25136cd52839408e12fe34fb
Leonid_Khristyuk <kasheyne@gmail.com>
2022-09-04 21:59:45 +0300

    Multi threading, Level 9, lecture 15 solutions.

--- src/com/javarush/task/task27/task2712/Restaurant.java
+++ src/com/javarush/task/task27/task2712/Restaurant.java
@@ -1,12 +1,13 @@
 package com.javarush.task.task27.task2712;
 
+import com.javarush.task.task27.task2712.kitchen.Cook;
+
 public class Restaurant {
 
     public static void main(String[] args) {
-        Tablet tablet = new Tablet(1);
-        tablet.createOrder();
-        tablet.createOrder();
-        tablet.createOrder();
+        Tablet tablet = new Tablet(5);
+        Cook cook = new Cook("Chef Ivlev");
+        tablet.addObserver(cook);
         tablet.createOrder();
     }
 
