65dcc84c6fd5e8b97b9c82139e0d42f6e97434aa
Leonid_Khristyuk <kasheyne@gmail.com>
2022-09-06 07:51:56 +0300

    Multi threading, Level 9, lecture 15 solutions.

--- src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
+++ src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
@@ -1,16 +1,32 @@
 package com.javarush.task.task27.task2712.ad;
 
-public final class AdvertisementStorage {
-    
-    private static AdvertisementStorage instance;
+import java.util.ArrayList;
+import java.util.List;
 
-    private AdvertisementStorage(){
+public final class AdvertisementStorage {
+
+    private static AdvertisementStorage instance;
+    private final List<Advertisement> videos = new ArrayList<>();
+
+    private AdvertisementStorage() {
+        Object someContent = new Object();
+        add(new Advertisement(someContent, "Baikal", 5000, 100, 3 * 60));
+        add(new Advertisement(someContent, "Tarhun", 100, 10, 15 * 60));
+        add(new Advertisement(someContent, "Pancake", 400, 2, 10 * 60));
     }
-    
-    public static AdvertisementStorage getInstance(){
+
+    public static AdvertisementStorage getInstance() {
         if (instance == null) {
             instance = new AdvertisementStorage();
         }
         return instance;
+    }
+
+    public List<Advertisement> list() {
+        return videos;
+    }
+
+    public void add(Advertisement advertisement) {
+        videos.add(advertisement);
     }
 }
