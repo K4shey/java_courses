13e42005ca214b0d58405d25d25399951344a527
Leonid_Khristyuk <kasheyne@gmail.com>
2022-11-02 23:00:05 +0300

    Multi threading, Level 9, lecture 15 solutions.

--- src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
+++ src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -1,10 +1,17 @@
 package com.javarush.task.task27.task2712.statistic;
 
 import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
+import com.javarush.task.task27.task2712.statistic.event.EventType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class StatisticManager {
 
     private static StatisticManager instance;
+    private StatisticManager.StatisticStorage statisticStorage = new StatisticStorage();
 
     private StatisticManager() {
 
@@ -18,5 +25,15 @@ public class StatisticManager {
     }
 
     public void register(EventDataRow data) {
+    }
+
+    private class StatisticStorage {
+        private Map<EventType, List<EventDataRow>> storage = new HashMap<>();
+
+        public StatisticStorage() {
+            for (EventType element : EventType.values()) {
+                storage.put(element, new ArrayList<EventDataRow>());
+            }
+        }
     }
 }
