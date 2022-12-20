fbc52bd41ad22f3f3e0861f19aa9e83baa0d06d6
K4shey <103300828+K4shey@users.noreply.github.com>
2022-12-20 22:16:37 +0300

    Delete src/ForTest/InitializationOfInerfaces directory

--- src/ForTest/InitializationOfInerfaces/Student.java
+++
@@ -1,8 +0,0 @@
-package ForTest.InitializationOfInerfaces;
-
-public class Student implements Study {
-    @Override
-    public void learn() {
-        System.out.println("Now time for fun, I'll learn later");
-    }
-}
