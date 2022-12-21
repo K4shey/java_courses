01ac15fc12cacef4d04a9aeacf6d3a19c75be1ff
Leonid_Khristyuk <kasheyne@gmail.com>
2022-12-21 10:55:54 +0300

    Java Collections, Level 3, lecture 5 solutions.

---
+++ src/com/javarush/task/task33/task3304/Solution.java
@@ -0,0 +1,76 @@
+package com.javarush.task.task33.task3304;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Locale;
+
+/*
+Конвертация из одного класса в другой используя JSON Ӏ 3304
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Second s = (Second) convertOneToAnother(new First(), Second.class);
+        First f = (First) convertOneToAnother(new Second(), First.class);
+    }
+
+    public static Object convertOneToAnother(Object one, Class resultClassObject) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        StringWriter stringWriter = new StringWriter();
+        String strResult = objectMapper.writeValueAsString(one);
+        strResult = strResult.replaceFirst(
+                one.getClass().getSimpleName().toLowerCase(Locale.ROOT),
+                resultClassObject.getSimpleName().toLowerCase(Locale.ROOT));
+        return objectMapper.readValue(strResult, resultClassObject);
+    }
+
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "className")
+    @JsonSubTypes(@JsonSubTypes.Type(value = First.class, name = "first"))
+    public static class First {
+        public int i;
+        public String name;
+    }
+
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "className")
+    @JsonSubTypes(@JsonSubTypes.Type(value = Second.class, name = "second"))
+    public static class Second {
+        public int i;
+        public String name;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
